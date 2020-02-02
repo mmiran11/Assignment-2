9c296889bbb12f2bfdfd4f567ef18c42dce75432
mmiran11 <mmiran11@students.kennesaw.edu>
2020-02-02 14:55:26 -0500

    created vacation class and all_inclusiveVacation class.
    made budget method in base class. overloaded subclass's constructor.

---
+++ src/All_InclusiveVacation.java
@@ -0,0 +1,26 @@
+
+public class All_InclusiveVacation extends Vacation {
+	protected String brand;
+	protected int rating, price;
+
+	public All_InclusiveVacation() {
+		budget = 1000;
+		destination = "Hawaii";
+		brand = "Hilton Resorts";
+		rating = 3;
+		price = 1000;
+	}
+	
+	public All_InclusiveVacation(int bud, String dest, String brd, int rate, int prc) {
+		budget = bud;
+		destination = dest;
+		brand = brd;
+		rating = rate;
+		price = prc;
+	}
+	
+	public @Override String toString() {
+		return "This is the All-InclusiveVacation class. Its variables are: " + budget + ", " + destination + ", " + brand + ", " + rating + ", and " + price; 
+	}
+
+}
---
+++ src/Vacation.java
@@ -0,0 +1,13 @@
+
+public class Vacation {
+	protected int budget;
+	protected String destination;
+	
+	public int Budget(int cost) {
+		
+	}
+	
+	public String toString() {
+		return "This is the Vacation class. Its variables are: " + budget + " and " + destination;
+	}
+}
