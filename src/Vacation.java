3bec1a6c1fb40931fb081b984608abbb5eef96d3
mmiran11 <mmiran11@students.kennesaw.edu>
2020-02-02 16:08:08 -0500

    created another budget method in base class.
    one budget method will have a parameter for an integer and the other
    will have a parameter for an array

--- src/Vacation.java
+++ src/Vacation.java
@@ -13,6 +13,20 @@ public class Vacation {
 		}
 	}
 	
+	public void Budget(int [] cost) {
+		int totalCost = 0;
+		for(int i = 0; i < cost.length; i++) {
+			totalCost += cost[i];
+		}
+		int difference = totalCost - budget;
+		if (difference <= 0) {
+			System.out.println("The vacation is $" + (difference * -1) + " under the budget.");
+		}
+		else {
+			System.out.println("The vacation is $" + difference + " over the budget.");
+		}
+	}
+	
 	public String toString() {
 		return "This is the Vacation class. The budget is " + budget + " and the destination is " + destination;
 	}
