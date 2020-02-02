af7457d05b906c2634434c8d5da72a1f4ed0e61b
mmiran11 <mmiran11@students.kennesaw.edu>
2020-02-02 16:23:09 -0500

    overloaded piecemealVacation's constructor

--- src/PiecemealVacation.java
+++ src/PiecemealVacation.java
@@ -7,6 +7,15 @@ public class PiecemealVacation extends Vacation {
 		budget = 1000;
 		destination = "Jamaica";
 	}
+	
+	public PiecemealVacation(int bud, String dest, String[] itms, int[] price) {
+		budget = bud;
+		destination = dest;
+		for(int i = 0; i < itms.length; i++) {
+			items[i] = itms[i];
+			costs[i] = price[i];
+		}
+	}
 
 	public @Override String toString() {
 		return "This is the PiecemealVacation class. The budget is " + budget + ", the destination is " + destination + ", the items needed are: a " + items[0] + " that costs $" + costs[0] + ", a " + items[1] + " that costs $" + costs[1] + ", and a " + items[2] + " that costs $" + costs[2]; 
